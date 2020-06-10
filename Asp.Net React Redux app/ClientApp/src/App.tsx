fa90e5ee025c0f5b0f7ed14b09390cf10d838770
Yatsutko Sergey <s3rzh@inbox.ru>
2020-06-10 23:29:04 +0700

    Init commit

---
+++ Asp.Net React Redux app/ClientApp/src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import './custom.sass'
+import Layout from "./components/Layout";
+import {Route} from "react-router";
+import Home from "./components/Home";
+import Counter from "./components/Counter";
+import FetchData from "./components/FetchData";
+
+export default () => {
+    return (
+        <Layout>
+            <Route exact path='/' component={Home}/>
+            <Route path='/counter' component={Counter}/>
+            <Route path='/fetch-data/:startDateIndex?' component={FetchData}/>
+        </Layout>
+    );
+}
